38809791fb47b99387cef283d6d4687f63890f41
Jakub Fik <fikkuba@gmail.com>
2021-04-12 15:14:56 +0200

    AbroadTrip class / getPrice override

--- src/main/java/model/AbroadTrip.java
+++ src/main/java/model/AbroadTrip.java
@@ -21,6 +21,11 @@ public class AbroadTrip extends Trip{
     }
 
     @Override
+    public BigDecimal getPrice() {
+        return super.getPrice().add(insurance);
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
