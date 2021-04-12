4460f5e54fd0a3be2aba96c04c590e02b5282af6
Jakub Fik <fikkuba@gmail.com>
2021-04-12 15:13:08 +0200

    AbroadTrip class

---
+++ src/main/java/model/AbroadTrip.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class AbroadTrip extends Trip{
+    private BigDecimal insurance;
+
+    public AbroadTrip(LocalDate startOfTrip, LocalDate endOfTrip, String destiny, BigDecimal price, BigDecimal insurance) {
+        super(startOfTrip, endOfTrip, destiny, price);
+        this.insurance = insurance;
+    }
+
+    public BigDecimal getInsurance() {
+        return insurance;
+    }
+
+    public void setInsurance(BigDecimal insurance) {
+        this.insurance = insurance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        AbroadTrip that = (AbroadTrip) o;
+        return Objects.equals(insurance, that.insurance);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), insurance);
+    }
+
+    @Override
+    public String toString() {
+        return "AbroadTrip{" +
+                "insurance=" + insurance +
+                '}';
+    }
+}
